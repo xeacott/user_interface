f94b1649dcd6ce6c1e92cb2cb61e0e2d6fedf3c0
Eacott <jeacott22@hotmail.com>
2019-01-22 11:20:42 -0500

    Added menu bar, menu items, persistent states, and groups.

--- app/src/main/java/com/example/user_interface/MainActivity.java
+++ app/src/main/java/com/example/user_interface/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user_interface;
 
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -66,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuCompat.setGroupDividerEnabled(menu, true);
         return true;
     }
 
@@ -74,14 +76,22 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch(item.getItemId()) {
+            case R.id.action_bluetooth_menu:
+            case R.id.action_start_session:
+            case R.id.action_end_session:
+                return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            case R.id.action_checkbox_ekg:
+            case R.id.action_checkbox_pulse_ox:
+
+                if (item.isChecked()) item.setChecked(false);
+                else item.setChecked(true);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
