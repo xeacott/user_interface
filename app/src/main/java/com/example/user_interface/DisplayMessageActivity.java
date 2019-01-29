dc86bd7b31f6488d86d5ac442cd06a1bff1d0f82
Joe Eacott <jeacott22@hotmail.com>
2019-01-28 21:56:44 -0500

    Merged bluetooth into menu_bar_items, added more functionality and cleaned up socket logic.

--- app/src/main/java/com/example/user_interface/DisplayMessageActivity.java
+++ app/src/main/java/com/example/user_interface/DisplayMessageActivity.java
@@ -3,9 +3,12 @@ package com.example.user_interface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +17,17 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
-        // Capture the layout's TextView and set the string as its text
-//        TextView textView = findViewById(R.id.editText);
-//        textView.setText(message);
+        String message = intent.getStringExtra(MainActivity.TAG);
+        Button button = (Button) findViewById(R.id.establish_connection);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "Coming from other page";
+                Intent intent = new Intent();
+                intent.putExtra("Test", message);
+                setResult(2, intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/user_interface/MainActivity.java
+++ app/src/main/java/com/example/user_interface/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.user_interface;
 
 import android.content.Intent;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.MenuCompat;
@@ -12,6 +16,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,25 +27,176 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.Set;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
-    public static final String EXTRA_MESSAGE = "com.example.user_interface.MESSAGE";
+    private BluetoothDevice mDevice;
+    private BluetoothSocket mSocket;
+    private static final UUID MY_UUID_INSECURE = UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
+    private Handler handler;
+    ConnectedThread mConnectedThread;
+
+    // Widget information
+    static String TAG = "MainActivity";
+    EditText send_data;
+    TextView view_data;
+    StringBuilder messages;
+
     public boolean electro__graph_bool= false;
     public boolean pulseox_graph_bool = false;
+
+    // Create a bluetooth adapter to get the devices bluetooth adapter
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+    /*
+    This hooks to "Establish Connection" UI button is Start Session. In order to begin
+    collecting information, bluetooth for both devices must be on. This method will
+    open a socket over bluetooth to connect each device.
+     */
+    public void pairDevice(int ... params) {
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        Log.e("MainActivity", "" + pairedDevices.size());
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices ) {
+                mDevice = device;
+            }
+
+            // Pass the paired device to create a socket between devices
+            BluetoothSocket tmp = null;
+            try {
+                tmp = mDevice.createInsecureRfcommSocketToServiceRecord(MY_UUID_INSECURE);
+            } catch (IOException e) {
+                Toast.makeText(this, "Could not establish socket, check UUID", Toast.LENGTH_SHORT).show();
+            }
+            mSocket = tmp;
+
+            // First open the socket by connecting it
+            try {
+                mSocket.connect();
+            } catch (IOException connectException) {
+                try {
+                    mSocket.close();
+                } catch (IOException closeException ) { }
+                Toast.makeText(this, "Device did not pair.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // Pass the open socket to a thread to send/receive messages and start it
+            ConnectedThread mConnectThread = new ConnectedThread(mSocket);
+            mConnectThread.start();
+        }
+    }
+
+    /*
+     Open a socket using a thread.
+     This thread is responsible for maintaining the BTconnection, sending the data,
+     and receiving incoming data through input/output streams.
+      */
+
+    // Allow messages to be sent back and forth using input stream and output stream.
+    // Note:: This is a thread that is continuously watching the socket to either
+    // receive information or send information.
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Obtain a handle to the socket's input and output streams
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+            }
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+
+            int begin = 0;
+            int bytes = 0; // bytes returned from read()
+            while (true) {
+                // Read from the InputStream
+                try {
+                    bytes += mmInStream.read(buffer, bytes, buffer.length - bytes);
+                    for(int i = begin; i < bytes; i++) {
+                        if(buffer[i] == "#".getBytes()[0]) {
+                            final String incomingMessage = new String(buffer, 0, bytes);
+                            Log.d(TAG, "InputStream: " + incomingMessage);
+
+                            /*
+                            Runs the specified action on the UI thread. If the current thread is the UI
+                            thread, then the action is executed immediately. If the current thread is
+                            not the UI thread, the action is posted to the event queue of the UI thread.
+                            */
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    // This is the exact spot where the UI can be updated in real time
+                                    // without slowing down the UI.
+                                    // See: https://developer.android.com/guide/components/processes-and-threads#java
+                                    view_data.setText(incomingMessage);
+                                }
+                            });
+                            begin = i +1;
+                            if(i == bytes -1) {bytes = 0; begin = 0;
+                                }
+                            }
+                        }
+                } catch (IOException e) {
+                    Log.e(TAG, "write: Error reading Input Stream. " + e.getMessage());
+                    break;
+                }
+            }
+        }
+
+        /* Do not call directly, call send message instead */
+        public void write(byte[] bytes) {
+            String text = new String(bytes, Charset.defaultCharset());
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+                Log.e(TAG, "write: Error writing to output stream. " + e.getMessage());
+            }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    /*
+    This method will send a message directly to the connected device and is currently unhooked.
+    It may be used to end a session, change parameters on the fly, etc.
+    This is the handle to the input stream from the live socket.
+     */
+    public void SendMessage(View v) {
+        byte[] bytes = send_data.getText().toString().getBytes(Charset.defaultCharset());
+        mConnectedThread.write(bytes);
+    }
+
+    public void EndConnection(View v) {
+        mConnectedThread.cancel();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,14 +222,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
+        if (mBluetoothAdapter == null) {
+            // Device does not support bluetooth
+            Toast.makeText(this, "Device does not support Bluetooth", Toast.LENGTH_SHORT).show();
+        }
 
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+    }
     public void onStartSessionClick(MenuItem item) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.action_start_session);
         String message = item.toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        intent.putExtra(TAG, message);
+        startActivityForResult(intent, 2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 2) {
+            pairDevice();
+            //Toast.makeText(this, "Came back from StartSession with params available.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onEndSessionClick(MenuItem item) {
@@ -133,9 +306,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
     public static class PlaceholderFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
@@ -168,10 +338,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
